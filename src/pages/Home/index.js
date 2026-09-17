fec9909abe55c994704b124197ded100e82a4336
Jean-Daniel Poulain <jeandpoulain@gmail.com>
2026-09-17 09:07:07 +0200

    corection bug filtre et footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -113,7 +113,7 @@ const Page = () => {
         </Modal>
       </div>
     </main>
-    <footer className="row">
+    <footer className="row" >
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
         {last && last.cover && (
